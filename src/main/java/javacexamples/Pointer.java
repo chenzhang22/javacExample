eb8aea088508aa1c5fb150060ce7f555b0bc0b73
Chen Zhang <340355960@qq.com>
2024-01-24 19:11:56 +0800

    1-24

--- src/main/java/javacexamples/Pointer.java
+++ src/main/java/javacexamples/Pointer.java
@@ -3,10 +3,37 @@ package javacexamples;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.Iterator;
 
 public class Pointer {
     public List list() {
         System.out.println("List");
         return new ArrayList<>();
     }
+
+    public static void main(String[] args) {
+		
+		//creating sample Collection
+		List<Integer> myList = new ArrayList<Integer>();
+		for(int i=0; i<10; i++) myList.add(i);
+		
+		//traversing using Iterator
+		Iterator<Integer> it = myList.iterator();
+		while(it.hasNext()){
+			Integer i = it.next();
+			System.out.println("Iterator Value::"+i);
+		}
+		
+		//traversing through forEach method of Iterable with anonymous class
+		myList.forEach(new Consumer<Integer>() {
+
+			public void accept(Integer t) {
+				System.out.println("forEach anonymous class Value::"+t);
+			}
+
+		});
+		
+	}
+
 }
